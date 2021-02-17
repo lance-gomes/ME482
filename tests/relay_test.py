bb5c9585d821f32ef1f22c69f9a180a224edc46d
Lance Gomes <lvgomes@edu.uwaterloo.ca>
2021-02-16 23:10:28 -0500

    Add relay test

---
+++ tests/relay_test.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as IO
+import time
+
+IO.setmode(IO.BOARD)
+IO.setup(19, IO.OUT)
+IO.setup(21, IO.OUT)
+
+
+def main():
+  while True:
+      IO.output(19, IO.HIGH)
+      IO.output(21, IO.HIGH)
+
+      time.sleep(2)
+
+      IO.output(19, IO.LOW)
+      IO.output(21, IO.LOW)
+
+      time.sleep(2)
+
+
+if __name__ == "__main__":
+  main()
