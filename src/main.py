ef4ea1be57249b7c5137f917ac88443fb55ea6a2
Lance Gomes <lvgomes@edu.uwaterloo.ca>
2021-03-24 22:03:52 -0400

    yes

--- src/main.py
+++ src/main.py
@@ -4,15 +4,15 @@ import time
 def main():
     #Initilize
 
-    IO.setmode(IO.BOARD)
+    IO.setmode(IO.BCM)
 
-    IO.setup(11,IO.IN) # sensor 1 - hand washer
-    IO.setup(13,IO.IN) # sensor 2 - hand washer 
-    IO.setup(15,IO.IN) # base sensor 
+    IO.setup(17,IO.IN) # sensor 1 - hand washer
+    IO.setup(27,IO.IN) # sensor 2 - hand washer 
+    IO.setup(22,IO.IN) # base sensor 
 
-    IO.setup(19, IO.OUT) # two way
-    IO.setup(21, IO.OUT) # three way
-    IO.setup(23, IO.OUT) # pump
+    IO.setup(10, IO.OUT) # two way
+    IO.setup(9, IO.OUT) # three way
+    IO.setup(11, IO.OUT) # pump
 
     three_way(False)
     water(False)
@@ -20,9 +20,9 @@ def main():
     isOffState = True 
 
     # HIGH == OFF
-    IO.output(23, IO.HIGH) # pump
-    IO.output(19, IO.HIGH) # pump
-    IO.output(21, IO.HIGH) # pump
+    IO.output(11, IO.HIGH) # pump
+    IO.output(10, IO.HIGH) # pump
+    IO.output(9, IO.HIGH) # pump
 
     # no noise from the 3 way (since its normally on the hand washer)
     # 2 way click 
@@ -36,11 +36,11 @@ def main():
     while True:
         # IO.input(x) returns true if path is clear
 
-        # print(IO.input(13) == 0)
+        # print(IO.input(27) == 0)
 
         # either IR sensor detects a hand, run hand washer
-        #while IO.input(11) == 0 or IO.input(13) == 0:
-        if IO.input(11) == 0:
+        #while IO.input(17) == 0 or IO.input(27) == 0:
+        if IO.input(17) == 0:
             # direct 3-way valve to hand wash line
             three_way(False)
             time.sleep(3) # we need to wait 6 sec until the 3way turns NOOOO
@@ -61,7 +61,7 @@ def main():
         water(False)
 
         # # run regular faucet - our setup only allows one or the other so we can just to elif here
-        # while IO.input(15) == 0:
+        # while IO.input(22) == 0:
         #     isOffState=False
         #     print("Sensor 3 active /n")
         #     three_way(True)
@@ -74,9 +74,9 @@ def main():
 def water(input):
     # CONFIRM ANGLES
     if not input: # confirm that these don't keep turning they stop at 90 degree 
-        IO.output(19, IO.HIGH)
+        IO.output(10, IO.HIGH)
     else:
-        IO.output(19, IO.LOW)
+        IO.output(10, IO.LOW)
 
 # Direct to the either soap line or regular line 
 # false -> hand washer  
@@ -85,10 +85,10 @@ def water(input):
 def three_way(input):
     if not input:
         # direct to the handwasher 
-        IO.output(21, IO.HIGH)
+        IO.output(9, IO.HIGH)
     else:
         # direct flow to main
-        IO.output(21, IO.LOW)
+        IO.output(9, IO.LOW)
 
 # Soap Cycle
 def soap():
@@ -100,9 +100,9 @@ def soap():
 # Turn on and off pump 
 def pump(input):
     if not input:
-        IO.output(23, IO.HIGH)
+        IO.output(11, IO.HIGH)
     else:
-        IO.output(23, IO.LOW)
+        IO.output(11, IO.LOW)
 
 if __name__ == "__main__":
     main()
